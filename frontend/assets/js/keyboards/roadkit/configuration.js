f29b87fc0c22282a64334481dadacf5c8561e689
Evan Sailer <esailer@asu.edu>
2017-12-22 13:43:01 -0600

    fixed roadkit configuration

--- frontend/assets/js/keyboards/roadkit/configuration.js
+++ frontend/assets/js/keyboards/roadkit/configuration.js
@@ -55,7 +55,7 @@ roadkit.configKeymap.keys = [
     "K00", "K01", "K02", "K03",
     "K10", "K11", "K12", "K13",
     "K20", "K21", "K22", "K23",
-    "K30", "K31", "K34", "K32"
+    "K30", "K31", "K32", "K33"
 ];
 
 roadkit.configKeymap.positions = [
