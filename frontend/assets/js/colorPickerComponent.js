05efea21e7b5ad6c51a1354855c2d05997b79280
Evan Sailer <esailer@asu.edu>
2018-09-16 19:36:35 -0500

    cleanup colorpicker component

--- frontend/assets/js/colorPickerComponent.js
+++ frontend/assets/js/colorPickerComponent.js
@@ -2,70 +2,12 @@ Vue.component('color-picker', {
 	props: ['value'],
   data: function () {
     return {
-    	edit: false,
-    	previewColor: 'rgb('+this.value.red+','+this.value.green+','+this.value.blue+')',
-      colorPickerModel: '#' + this.decToHex(this.value.red) + this.decToHex(this.value.green) + this.decToHex(this.value.blue)
+    	edit: false
     }
   },
   methods: {
     input: function () {
       this.$emit('input', this.value);
-    },
-  	updatePreviewStyle: function() {
-    	this.previewColor = 'rgb('+this.value.red+','+this.value.green+','+this.value.blue+')';
-      this.colorPickerModel = '#' + this.decToHex(this.value.red) + this.decToHex(this.value.green) + this.decToHex(this.value.blue);
-      this.input();
-    },
-    updateRGB: function() {
-    	this.value.red = this.hexToDec(this.colorPickerModel.substring(1,3));
-      this.value.green = this.hexToDec(this.colorPickerModel.substring(3,5));
-      this.value.blue = this.hexToDec(this.colorPickerModel.substring(5,7));
-      this.updatePreviewStyle();
-    },
-    decToHex: function(dec) {
-    	var DEC_MAP = {
-      	0: '0',
-        1: '1',
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-        6: '6',
-        7: '7',
-        8: '8',
-        9: '9',
-        10: 'A',
-        11: 'B',
-        12: 'C',
-        13: 'D',
-        14: 'E',
-        15: 'F'
-      };
-
-      return DEC_MAP[parseInt(dec/16)] + DEC_MAP[dec%16];
-    },
-    hexToDec: function(hex) {
-    	hex = hex.toUpperCase();
-    	var HEX_MAP = {
-      	'0': 0,
-        '1': 1,
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        'A': 10,
-        'B': 11,
-        'C': 12,
-        'D': 13,
-        'E': 14,
-        'F': 15
-      };
-
-      return (HEX_MAP[hex[1]] + HEX_MAP[hex[0]] * 16);
     }
   },
   template: `
@@ -74,9 +16,9 @@ Vue.component('color-picker', {
 
     <div class="colorpicker" v-if="edit">
       <div class="controls">
-        <div><label>R</label> <input type="range" min="0" max="255" id="rVal" v-model="value.red" @input="updatePreviewStyle" /><input type="number" min="0" max="255" id="rValText" v-model="value.red" @input="updatePreviewStyle" /></div>
-        <div><label>G</label> <input type="range" min="0" max="255" id="gVal" v-model="value.green" @input="updatePreviewStyle" /><input type="number" min="0" max="255" id="gValText" v-model="value.green" @input="updatePreviewStyle" /></div>
-        <div><label>B</label> <input type="range" min="0" max="255" id="bVal" v-model="value.blue" @input="updatePreviewStyle" /><input type="number" min="0" max="255" id="bValText" v-model="value.blue" @input="updatePreviewStyle" /></div>
+        <div><label>R</label> <input type="range" min="0" max="255" id="rVal" v-model="value.red" @input="input" /><input type="number" min="0" max="255" id="rValText" v-model="value.red" @input="input" /></div>
+        <div><label>G</label> <input type="range" min="0" max="255" id="gVal" v-model="value.green" @input="input" /><input type="number" min="0" max="255" id="gValText" v-model="value.green" @input="input" /></div>
+        <div><label>B</label> <input type="range" min="0" max="255" id="bVal" v-model="value.blue" @input="input" /><input type="number" min="0" max="255" id="bValText" v-model="value.blue" @input="input" /></div>
       </div>
     </div>
   </div>
