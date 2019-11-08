ef80cd6597adf8338e83902a663350f7eb688968
evan s <esailer@asu.edu>
2019-11-08 10:50:06 -0700

    use env port

--- qmk-configurator-server/src/index.js
+++ qmk-configurator-server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const f = require('./firmware.js');
 const app = express();
+const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, '/../../frontend')));
 app.use(express.static(path.join(__dirname, '/../../qmk_firmware')));
@@ -51,6 +52,6 @@ app.post('/build', (req, res) => {
   });
 });
 
-app.listen(9000, () => {
-  console.log('app listening on port 9000');
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
 });
