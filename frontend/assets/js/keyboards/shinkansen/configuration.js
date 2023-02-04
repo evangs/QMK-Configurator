ae4843a010a74ab1cf82383cb6717c3fe5eeb170
evan s <esailer@asu.edu>
2023-02-03 23:51:40 -0700

    fix bug

--- frontend/assets/js/keyboards/shinkansen/configuration.js
+++ frontend/assets/js/keyboards/shinkansen/configuration.js
@@ -69,7 +69,7 @@ shinkansen.configKeymap.positions = [
     ["K00", "K01", "K02", "K03", "K04", "K05", "K06", "K07", "K08", "K09", "K0A", "K0B", "K0C", "K0D", "K0E", "K0F"],
     ["K10", "K11", "K12", "K13", "K14", "K15", "K16", "K17", "K18", "K19", "K1A", "K1B", "K1C", "KC_NO", "KC_NO", "KC_NO"],
     ["K20", "K21", "K22", "K23", "K24", "K25", "K26", "K27", "K28", "K29", "K2A", "K2B", "K2C", "KC_NO", "K2E", "KC_NO"],
-    ["K30", "K31", "K32", "KC_NO", "KC_NO", "KC_NO", "KC_36", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "K3B", "K3C", "K3D", "K3E", "K3F"]
+    ["K30", "K31", "K32", "KC_NO", "KC_NO", "KC_NO", "K36", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "K3B", "K3C", "K3D", "K3E", "K3F"]
 ];
 
 shinkansen.zones = [];
